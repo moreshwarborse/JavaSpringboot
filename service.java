ffa8600e4a89c9bf9e25ea9781035550fb58289c
moreshwarborse <134684632+moreshwarborse@users.noreply.github.com>
2025-04-10 18:36:54 +0530

    Service.java

--- service.java
+++ service.java
@@ -1,22 +1,34 @@
-package com.example.sample.service;
+package com.example.demo.services;
 
-import com.example.sample.entity.DataEntry;
-
-import com.example.sample.repository.repo;
+import com.example.demo.entity.DataEntry;
+import com.example.demo.entity.User;
+import com.example.demo.repository.Repo;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Component
 public class service {
-    //controller ----> Services ------> Repository
+        //controller ----> Services ------> Repository
     @Autowired
-    private repo repository;
+    private Repo repository;
 
-    public void save(DataEntry dataEntry){
+    @Autowired
+    private UserService userService;
+
+    public  void saveEntry(DataEntry dataEntry, String userName){
+        User user= userService.findByUserName(userName);
+        dataEntry.setDate(LocalDateTime.now() );
+        DataEntry saved = repository.save(dataEntry);
+        user.getEntries().add(saved);
+        userService.saveEntry(user);
+    }
+
+    public  void saveEntry(DataEntry dataEntry){
         repository.save(dataEntry);
     }
 
@@ -28,8 +40,10 @@ public class service {
         return  repository.findById(id);
     }
 
-    public  void delete(ObjectId id){
+    public  void deleteEntry(ObjectId id, String userName){
+        User user= userService.findByUserName(userName);
+        user.getEntries().removeIf(x -> x.getId().equals(id));
+        userService.saveEntry(user);
         repository.deleteById(id);
     }
-    //controller ----> service ----> repository
 }
